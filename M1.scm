da0da53240301b4ea9be6a11d9951cf6522a3179
SwitchAxe <sofiacerasuoli@gmail.com>
2021-07-19 23:31:58 +0200

    initial commit, added first automaton

---
+++ M1.scm
@@ -0,0 +1,31 @@
+(define (m1-aux string current-state)
+    (cond 
+        ((equal? (string->list string) '())
+            (if (= current-state 1)
+                (display "this string has been accepted!")
+                (display "this string has been rejected.")))
+        ((equal? (car (string->list string)) #\0)
+        ;;the car character in the remaining string is a '0', and 
+        ;;we need to check for the state the automaton is currently in:
+            (cond 
+                ((= current-state 0) 
+                    (m1-aux (list->string (cdr (string->list string))) 0))
+                ((= current-state 1)
+                    (m1-aux (list->string (cdr (string->list string))) 2))
+                ((= current-state 2)
+                    (m1-aux (list->string (cdr (string->list string))) 1))))   
+        ((equal? (car (string->list string)) #\1)
+            ;;the car character in the remaining string is a '1', and
+            ;;we need to check for the state the automaton is currently in:
+            (cond
+                ((= current-state 0)
+                    (m1-aux (list->string (cdr (string->list string))) 1))
+                ((= current-state 1)
+                    (m1-aux (list->string (cdr (string->list string))) 1))
+                ((= current-state 2)
+                    (m1-aux (list->string (cdr (string->list string))) 1))))
+        (else
+            (display "the string must contain only 1s or 0s!"))))
+
+(define (m1 string)
+    (m1-aux string 0))
